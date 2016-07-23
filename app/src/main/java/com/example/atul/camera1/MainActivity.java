0b5219e2bb3044a7a1968dd2a94174a5600b659c
Atul Rathore <kshtriya.atul@gmail.com>
2016-07-24 01:16:09 +0530

    Second  Commit with some changes

--- app/src/main/java/com/example/atul/camera1/MainActivity.java
+++ app/src/main/java/com/example/atul/camera1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.atul.camera1;
 
+import android.app.WallpaperManager;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
 
     Button button;
     ImageView imageView;
+    Bitmap bmp;
+    int a =1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +26,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivity(i);
+                startActivityForResult(i,a);
+
+//                WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
             }
         });
+
     }
+
+    protected void onActivityResult(int reqcode, int resultcode, Intent data){
+        super.onActivityResult(reqcode,resultcode,data);
+        if(reqcode==1){
+            Bundle b = data.getExtras();
+            bmp = (Bitmap) b.get("data");
+            imageView.setImageBitmap(bmp);
+        }
+    }
+
 }
